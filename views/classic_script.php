0916312749d90dbc24f43c0e5cb3ff6f4fcb8da5
ank91 <ank91@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-04 15:29:12 +0000

    tag 0.8.1
    git-svn-id: https://plugins.svn.wordpress.org/ank-simplified-ga/tags/0.8.1@1192457 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/classic_script.php
@@ -0,0 +1,24 @@
+<!--Classic GA Tracking start<?php if($debug_mode === true) {?>, Debugging is on<?php }?>-->
+<script type="text/javascript">
+<?php
+if($js_load_later === 1){ ?>
+function _loadGA(){
+<?php } ?>
+var _gaq = _gaq || [];
+<?php
+echo $ela_plugin_url;
+foreach($gaq as $item){
+echo "_gaq.push([" . $item . "]);\n";
+}
+?>
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = <?php echo $ga_src; ?>;
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+<?php if($js_load_later === 1) {?>
+}
+window.addEventListener?window.addEventListener("load",_loadGA,!1):window.attachEvent?window.attachEvent("onload",_loadGA):window.onload=_loadGA;
+<?php }?>
+</script>
+<!--GA Tracking ends-->
